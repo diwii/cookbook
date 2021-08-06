48c750f2179c7551f267889b3c1c6144d5f165c2
Valdis Locs <valdis.locs@gmail.com>
2021-08-06 23:15:22 +0300

    Add web interface

--- webrtc/peer-a.js
+++ webrtc/peer-a.js
@@ -1,15 +1,70 @@
 // Peer A is initiator
 
+let offer = '';
+
+const notifications = document.querySelector('#notification');
+
+const messageInput = document.querySelector('#message');
+const buttonSendMessage = document.querySelector('#send-message');
+buttonSendMessage.addEventListener('click', sendMessage);
+
+const answerInput = document.querySelector('#answer');
+const buttonSetAnswer = document.querySelector('#set-answer');
+buttonSetAnswer.addEventListener('click', setAnswer);
+
+const buttonCreateOffer = document.querySelector('#create-offer');
+buttonCreateOffer.addEventListener('click', createNewOffer);
+
 // local connection ??
 const localConnection = new RTCPeerConnection();
 const dataChannel = localConnection.createDataChannel('Channel name');
 
-dataChannel.onmessage = (event) => console.log('Just go a message: ' + event.data);
-dataChannel.onopen = () => console.log('Connection open');
+dataChannel.onmessage = (event) => {
+    pushNotification(event.data);
+    console.log('Just go a message: ' + event.data)
+};
+dataChannel.onopen = () => {
+    pushNotification('Connection open');
+    console.log('Connection open')
+};
 
 localConnection.onicecandidate = () => {
+    pushNotification(JSON.stringify(localConnection.localDescription));
     console.log('New ICE candidate reprinting SDP' + JSON.stringify(localConnection.localDescription));
 };
+
+function pushNotification(message) {
+    const copyText = () => {
+        const placeholderInput = document.createElement('input');
+        placeholderInput.value = message;
+        document.body.prepend(placeholderInput);
+        placeholderInput.select();
+        document.execCommand("copy");
+        placeholderInput.remove();
+    }
+
+    let listItem = document.createElement('li');
+    listItem.addEventListener('click', copyText);
+    listItem.innerText = message;
+    notification.append(listItem);
+}
+
+function createNewOffer() {
+    localConnection.createOffer()
+    .then(
+        offer => localConnection.setLocalDescription(offer)
+    ).then(
+        () => pushNotification('Offer created')
+    );
+}
+
+function setAnswer() {
+    localConnection.setRemoteDescription(JSON.parse(answerInput.value));
+}
+
+function sendMessage() {
+    dataChannel.send(messageInput.value);
+}
 
 // Offer is SDP (Session Description Protocol)
 // localConnection.createOffer()
--- webrtc/peer-b.js
+++ webrtc/peer-b.js
@@ -1,15 +1,73 @@
 // Peer B is reciever
+
+const notifications = document.querySelector('#notification');
+
+const messageInput = document.querySelector('#message');
+const buttonSendMessage = document.querySelector('#send-message');
+buttonSendMessage.addEventListener('click', sendMessage);
+
+const offerInput = document.querySelector('#offer');
+const buttonSetOffer = document.querySelector('#set-offer');
+buttonSetOffer.addEventListener('click', setOffer);
+
+const buttonCreateAnswer = document.querySelector('#create-answer');
+buttonCreateAnswer.addEventListener('click', createAnswer);
+
 const remoteConnection = new RTCPeerConnection();
 
 remoteConnection.onicecandidate = () => {
+    pushNotification(JSON.stringify(remoteConnection.localDescription));
     console.log('New ICE candidate reprinting SDP' + JSON.stringify(remoteConnection.localDescription));
 };
 
 remoteConnection.ondatachannel = (event) => {
     remoteConnection.dataChannel = event.channel;
-    remoteConnection.dataChannel.onmessage = (event) => console.log('New message from client' + event.data);
-    remoteConnection.dataChannel.onopen = () => console.log('Connection Opened!');
+    remoteConnection.dataChannel.onmessage = (event) => {
+        pushNotification(event.data);
+        console.log('New message from client' + event.data)
+    };
+    remoteConnection.dataChannel.onopen = () => {
+        pushNotification('Connection Opened!');
+        console.log('Connection Opened!')
+    };
 };
+
+function pushNotification(message) {
+    const copyText = () => {
+        const placeholderInput = document.createElement('input');
+        placeholderInput.value = message;
+        document.body.prepend(placeholderInput);
+        placeholderInput.select();
+        document.execCommand("copy");
+        placeholderInput.remove();
+    }
+
+    let listItem = document.createElement('li');
+    listItem.addEventListener('click', copyText);
+    listItem.innerText = message;
+    notification.append(listItem);
+}
+
+function setOffer() {
+    console.log(offerInput.value);
+    remoteConnection.setRemoteDescription(JSON.parse(offerInput.value))
+        .then(
+            () => pushNotification('Offer set')
+        );
+}
+
+function createAnswer() {
+    remoteConnection.createAnswer()
+    .then(
+        (answer) => remoteConnection.setLocalDescription(answer)
+    ).then(
+        () => pushNotification('Answer created')
+    );
+}
+
+function sendMessage() {
+    remoteConnection.dataChannel.send(messageInput.value);
+}
 
 // offer is SDP from initiator
 
